6db1c91c7fd8095b5afd8c058115dd8855f5754f
byby9999 <byby9999@users.noreply.github.com>
2019-07-31 17:21:27 +0300

    create

---
+++ CvBuilder/Controllers/BuilderController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CvBuilder.Controllers
+{
+    public class BuilderController : Controller
+    {
+        // GET: Builder
+        public ActionResult Index()
+        {
+            return RedirectToAction("AddDataStep1");
+        }
+
+        // GET: Builder/Details/5
+        public ActionResult Editor()
+        {
+            return View();
+        }
+    }
+}
