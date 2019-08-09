13bde7f091876ae54216943690c77bd5c1d5a51b
byby9999 <byby9999@users.noreply.github.com>
2019-08-09 17:00:34 +0300

    Multiple work items

---
+++ CvBuilder/Scripts/work.js
@@ -0,0 +1,16 @@
+﻿$("#addWorkItem").click(function () {
+
+    var firstWorkItemHidden = $(".work-item:hidden").first();
+    if (firstWorkItemHidden !== null && firstWorkItemHidden !== undefined) {
+        
+        firstWorkItemHidden.show();
+
+        var contentOfCurrentTab = firstWorkItemHidden.closest(".content");
+        contentOfCurrentTab.css('max-height', '');
+        contentOfCurrentTab.css('height', 'auto');
+    }
+    
+    var visibleWorkItems = $(".work-item:visible");
+    if (visibleWorkItems.length == 10)
+        $("#addWorkItem").css('disabled', 'disabled');
+});
